ce54767997d54f251b87cc01bd308b255836afb3
farrukh <farrukhanjum203@gmail.com>
2023-08-04 20:58:46 +0500

    updated date and override model save method

--- generic_based_view/my_app/serializer.py
+++ generic_based_view/my_app/serializer.py
@@ -14,3 +14,13 @@ class StudentSerializer(serializers.ModelSerializer):
     #     instance.stu_updated_at = validated_data.get('stu_updated_at', datetime.datetime.now())
     #     instance.save()
     #     return instance
+
+    # def validated_data(self):
+    #     if self.data['age'] < 1 or self.data['age'] > 50:
+    #         raise serializers.ValidationError('Student age should be between 1 and 100 year')
+    #     return self.data
+
+    def validate(self, attrs):
+        if attrs['age'] > 50 or attrs['age'] < 1:
+            raise serializers.ValidationError("Student age should be between 1 to 50 years")
+        return attrs
